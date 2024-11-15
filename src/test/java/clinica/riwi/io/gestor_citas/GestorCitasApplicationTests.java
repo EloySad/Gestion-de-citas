707cc5ad92cdf242f12bb6993d6d184aefc84e85
EloySad <johaneloysanchezhernandez@gmail.com>
2024-11-15 17:03:47 -0500

    feat: creates project and crud for users

---
+++ src/test/java/clinica/riwi/io/gestor_citas/GestorCitasApplicationTests.java
@@ -0,0 +1,13 @@
+package clinica.riwi.io.gestor_citas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorCitasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
